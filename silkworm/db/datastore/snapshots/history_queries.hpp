17d6835bb5b8d99e1a7876756791c5f7fd1ac6ff
somnathb1 <snb895@outlook.com>
2025-07-21 02:56:25 +0400

    Remove dead weight

--- silkworm/db/datastore/snapshots/history_queries.hpp
+++
@@ -1,7 +0,0 @@
-// Copyright 2025 The Silkworm Authors
-// SPDX-License-Identifier: Apache-2.0
-
-#pragma once
-
-#include "history_get_query.hpp"
-#include "history_range_in_period_query.hpp"
--- silkworm/db/datastore/snapshots/inverted_index_queries.hpp
+++
@@ -1,6 +0,0 @@
-// Copyright 2025 The Silkworm Authors
-// SPDX-License-Identifier: Apache-2.0
-
-#pragma once
-
-#include "inverted_index_range_by_key_query.hpp"
